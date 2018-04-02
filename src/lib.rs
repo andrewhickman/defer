cb37f55947e0e0dd4358b312f4661af13a7986f0
Andrew Hickman <andrew.hickman1@sky.com>
2018-04-02 22:00:38 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+struct Defer<F: FnOnce()>(Option<F>);
+
+impl<F: FnOnce()> Drop for Defer<F> {
+    fn drop(&mut self) {
+        self.0.take().map(|f| f());
+    }
+}
+
+/// Defer execution of a closure until the return value is dropped.
+pub fn defer<F: FnOnce()>(f: F) -> impl Drop {
+    Defer(Some(f))
+}
+
+#[test]
+fn test() {
+    use std::cell::RefCell;
+
+    let i = RefCell::new(0);
+
+    {
+        let _d = defer(|| *i.borrow_mut() += 1);
+        assert_eq!(*i.borrow(), 0);
+    }
+
+    assert_eq!(*i.borrow(), 1);
+}
