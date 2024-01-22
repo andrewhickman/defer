ca2c1c372416a8dd1ff375ba6e0c3f0465370f78
Berkus Decker <berkus+github@metta.systems>
2024-01-22 23:08:06 +0200

    feat: Modernize implementation, add defer! macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-struct Defer<F: FnOnce()>(Option<F>);
-
-impl<F: FnOnce()> Drop for Defer<F> {
-    fn drop(&mut self) {
-        self.0.take().map(|f| f());
-    }
-}
+use core::mem::ManuallyDrop;
 
 /// Defer execution of a closure until the return value is dropped.
-pub fn defer<F: FnOnce()>(f: F) -> impl Drop {
-    Defer(Some(f))
+pub fn defer<F>(f: F) -> impl Drop where F: FnOnce() {
+    struct Defer<F: FnOnce()>(ManuallyDrop<F>);
+    
+    impl<F: FnOnce()> Drop for Defer<F> {
+        fn drop(&mut self) {
+            let f: F = unsafe { ManuallyDrop::take(&mut self.0) };
+            f();
+        }
+    }
+    
+    Defer(ManuallyDrop::new(f))
+}
+
+/// Defer execution of a closure until the current scope end.
+#[macro_export]
+macro_rules! defer {
+    ($e:expr) => {
+        let _defer = $crate::defer(|| $e);
+    };
 }
 
 #[test]
@@ -19,6 +30,20 @@ fn test() {
 
     {
         let _d = defer(|| *i.borrow_mut() += 1);
+        assert_eq!(*i.borrow(), 0);
+    }
+
+    assert_eq!(*i.borrow(), 1);
+}
+
+#[test]
+fn test_macro() {
+    use std::cell::RefCell;
+
+    let i = RefCell::new(0);
+
+    {
+        defer!(*i.borrow_mut() += 1);
         assert_eq!(*i.borrow(), 0);
     }
 
